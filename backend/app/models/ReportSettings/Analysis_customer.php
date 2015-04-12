e05d10bfe51f222ad1d1176933a36e1e7e20d5cf
hong304 <hong304@gmail.com>
2015-04-12 13:55:19 +0800

    custoemr report bug fix

--- backend/app/models/ReportSettings/Analysis_customer.php
+++ backend/app/models/ReportSettings/Analysis_customer.php
@@ -31,42 +31,46 @@ class Analysis_customer {
         $accu = [];
         $a_data = [];
         $current_year = date('Y');
-        $last_year = date('Y')-1;
+        $last_year = date('Y') - 1;
 
         // get invoice from that date and that zone
 
         // $reports = data_product::with('datawarehouse_product')->where('id',$this->_product_id)->first();
-        $reports = datawarehouse_customer::with('Customer')->where('customer_id',$this->_client_id)->where(function($query){
-            $query->where('year',date('Y'))
-                ->orWhere('year', date('Y')-1);
+        $reports = datawarehouse_customer::with('Customer')->where('customer_id', $this->_client_id)->where(function ($query) {
+            $query->where('year', date('Y'))
+                ->orWhere('year', date('Y') - 1);
         })->get();
 
-       // pd($reports);
 
         $amount = 0;
         $qty = 0;
+
+        for ($i = 1; $i <= 13; $i++) {
+            $a_data[$i] = '';
+        }
+
+
+        if (count($reports) == 0) {
+            $this->data = $a_data;
+            return $this->data;
+        }
 
         $product_name = $reports[0]->customer->customerName_chi;
         $product_id = $reports[0]->customer->customerId;
         $address = $reports[0]->customer->address_chi;
         $contact = $reports[0]->customer->phone_1;
 
-        for($i=1;$i<=12;$i++){
-            $a_data[$i] = '';
-        }
 
-
-
-        for($i=1;$i<=12;$i++){
-            foreach($reports as $v){
-                if($v->month == $i){
+        for ($i = 1; $i <= 12; $i++) {
+            foreach ($reports as $v) {
+                if ($v->month == $i) {
                     $a_data[$i][$v->year] = $v;
                 }
             }
         }
 
-        foreach($a_data as $v){
-            if(isset($v[$last_year])){
+        foreach ($a_data as $v) {
+            if (isset($v[$last_year])) {
                 $amount += $v[$last_year]['amount'];
                 $qty += $v[$last_year]['qty'];
             }
@@ -77,14 +81,14 @@ class Analysis_customer {
         $maxsingle = -999999;
         $found_item = null; //will hold item with max val;
 
-        foreach($a_data as $k=>$v)
-            if(isset($v[$last_year])) {
+        foreach ($a_data as $k => $v)
+            if (isset($v[$last_year])) {
                 if ($v[$last_year]['amount'] > $max)
                     $max = $v[$last_year]['amount'];
-                if($v[$last_year]['qty']>$maxqty)
+                if ($v[$last_year]['qty'] > $maxqty)
                     $maxqty = $v[$last_year]['qty'];
-                if($v[$last_year]['amount']/$v[$last_year]['qty']>$maxsingle)
-                    $maxsingle = $v[$last_year]['amount']/$v[$last_year]['qty'];
+                if ($v[$last_year]['amount'] / $v[$last_year]['qty'] > $maxsingle)
+                    $maxsingle = $v[$last_year]['amount'] / $v[$last_year]['qty'];
             }
 
 
@@ -96,7 +100,7 @@ class Analysis_customer {
             'highest_qty' => $maxqty,
             'highest_single' => $maxsingle,
         ];
-        $accu[13][$last_year]=$a_data[13][$last_year];
+        $accu[13][$last_year] = $a_data[13][$last_year];
 
         $max = -9999999; //will hold max val
         $maxqty = -99999999;
@@ -105,29 +109,40 @@ class Analysis_customer {
         $qty = 0;
 
 
-        foreach($a_data as $v){
-            if(isset($v[$current_year])){
+        foreach ($a_data as $v) {
+            if (isset($v[$current_year])) {
                 $amount += $v[$current_year]['amount'];
                 $qty += $v[$current_year]['qty'];
             }
         }
 
-        foreach($a_data as $k=>$v)
-            if(isset($v[$current_year])){
-                if($v[$current_year]['amount']>$max)
+        foreach ($a_data as $k => $v)
+            if (isset($v[$current_year])) {
+                if ($v[$current_year]['amount'] > $max)
                     $max = $v[$current_year]['amount'];
-                if($v[$current_year]['qty']>$maxqty)
+                if ($v[$current_year]['qty'] > $maxqty)
                     $maxqty = $v[$current_year]['qty'];
-                if($v[$current_year]['amount']/$v[$current_year]['qty']>$maxsingle)
-                    $maxsingle = $v[$current_year]['amount']/$v[$current_year]['qty'];
+                if ($v[$current_year]['amount'] / $v[$current_year]['qty'] > $maxsingle)
+                    $maxsingle = $v[$current_year]['amount'] / $v[$current_year]['qty'];
             }
 
-        $i = Invoice::with('staff','client')->where('customerId',$this->_client_id)->Orderby('deliveryDate','dest')->first();
+        $i = Invoice::with('staff', 'client')->where('customerId', $this->_client_id)->Orderby('deliveryDate', 'dest')->first();
 
 
+        if (isset($i->client->created_at))
+            $created_date = date('d-m-Y', $i->client->created_at);
+        else
+            $created_date = '';
 
+        if (!isset($i->deliveryDate)) {
+            $deliveryDate = '';
+            $lasttonow = '';
+        } else {
+                     $deliveryDate = date('d-m-Y', $i->deliveryDate);
+            $lasttonow = floor((time() - $i->deliveryDate) / 86400);
+    }
 
-        $a_data[13][$current_year] = [
+            $a_data[13][$current_year] = [
             'amount' => $amount,
             'qty' => $qty,
             'month' => '',
@@ -139,13 +154,13 @@ class Analysis_customer {
             'address' => $address,
             'contact' => $contact,
 
-            'craete_date' =>  date('d-m-Y', $i->client->created_at),
-            'last_time' => date('d-m-Y', $i->deliveryDate),
-            'last_to_now' => floor((time() - $i->deliveryDate) / 86400),
-            'saleman' => $i->staff->name,
-            'area_id' => $i->zoneId,
-            'area' => $i->zoneText,
-            'paymentTerm' => $i->client->paymentTermText
+            'craete_date' => $created_date,
+            'last_time' => $deliveryDate,
+            'last_to_now' => $lasttonow,
+            'saleman' => isset($i->staff->name)?$i->staff->name:'' ,
+            'area_id' => isset($i->zoneId)?$i->zoneId: '',
+            'area' => isset($i->zoneText)?$i->zoneText: '',
+            'paymentTerm' => isset($i->client->paymentTermText)?$i->client->paymentTermText:''
 
         ];
 
