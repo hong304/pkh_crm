886f43370a01798ad98a3a983315c9b8d4541ac4
hong304 <hong304@gmail.com>
2015-04-21 13:05:08 +0800

    printing improvement

--- backend/app/controllers/ReportController.php
+++ backend/app/controllers/ReportController.php
@@ -45,9 +45,17 @@ class ReportController extends BaseController {
 
         $report = ReportArchive::where('id', $rid)->first();
 
-       $url = CloudStorageTools::getPublicUrl($report->file, false);
-        
-       return Redirect::to($url);
+        $content = file_get_contents($report->file);
+
+        header('Content-Type: application/pdf');
+        header('Content-Length: '.strlen( $content ));
+        header('Content-disposition: inline; filename="' . $report->file . '"');
+        header('Cache-Control: public, must-revalidate, max-age=0');
+        header('Pragma: public');
+        header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+        header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+        echo $content;
         
     }
 }
