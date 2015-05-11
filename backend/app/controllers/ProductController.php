243048b027e11eb7e153ba8ebf87275e154d9f54
hong304 <hong304@gmail.com>
2015-05-11 10:55:10 +0800

    search product

--- backend/app/controllers/ProductController.php
+++ backend/app/controllers/ProductController.php
@@ -144,7 +144,7 @@ class ProductController extends BaseController {
 
 
                                     ->where('productStatus','o')
-                                    ->limit(1)->get();
+                                    ->limit(30)->get();
              
         }
         
