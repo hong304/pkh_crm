9d37a195e33a01d6831995ffb8f3418ad642c69a
hong304 <hong304@gmail.com>
2015-05-19 16:22:55 +0800

    cost price function

--- backend/app/views/reports/CashReceiptSummary.php
+++ backend/app/views/reports/CashReceiptSummary.php
@@ -30,37 +30,6 @@
     </tbody>
 </table>
 
-<table class="table table-bordered table-hover" style="font-size:15px;">
-    <thead>
-    <tr role="row" class="heading">
-        <th width="100%" colspan="4">未收款項</th>
-    </tr>
-    </thead>
-
-    <thead>
-    <tr role="row" class="heading">
-        <th width="20%">訂單</th>
-        <th width="50%">客戶</th>
-        <th width="15%">應收金額</th>
-        <th width="15%">累計</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach($backaccount as $row):?>
-        <tr>
-            <td><?php echo $row['invoiceNumber']; ?></td>
-            <td><?php echo $row['name']; ?></td>
-            <td>HK$ <?php echo $row['amount']; ?></td>
-            <td>HK$ <?php echo $row['accumulator']; ?></td>
-        </tr>
-    <?php endforeach; ?>.
-    <tr>
-        <td colspan="4" style="text-align:right;">
-            <span style="font-weight:bold;font-size:15px;">總計: HK$<?php echo end($backaccount)['accumulator']; ?></span>
-        </td>
-    <tr>
-    </tbody>
-</table>
 
 
 <table class="table table-bordered table-hover" style="font-size:15px;">
@@ -96,3 +65,36 @@
     <tr>
     </tbody>
 </table>
+
+<table class="table table-bordered table-hover" style="font-size:15px;">
+    <thead>
+    <tr role="row" class="heading">
+        <th width="100%" colspan="4">未收款項</th>
+    </tr>
+    </thead>
+
+    <thead>
+    <tr role="row" class="heading">
+        <th width="20%">訂單</th>
+        <th width="50%">客戶</th>
+        <th width="15%">應收金額</th>
+        <th width="15%">累計</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($backaccount as $row):?>
+        <tr>
+            <td><?php echo $row['invoiceNumber']; ?></td>
+            <td><?php echo $row['name']; ?></td>
+            <td>HK$ <?php echo $row['amount']; ?></td>
+            <td>HK$ <?php echo $row['accumulator']; ?></td>
+        </tr>
+    <?php endforeach; ?>.
+    <tr>
+        <td colspan="4" style="text-align:right;">
+            <span style="font-weight:bold;font-size:15px;">總計: HK$<?php echo end($backaccount)['accumulator']; ?></span>
+        </td>
+    <tr>
+    </tbody>
+</table>
+
