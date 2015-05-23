cbb5c5202e84a2219f77e3acf84063f83e963adf
hong304 <hong304@gmail.com>
2015-05-23 11:53:57 +0800

    commission

--- backend/app/controllers/CommissionController.php
+++ backend/app/controllers/CommissionController.php
@@ -1,75 +1,65 @@
 <?php
 
-class CommissionController extends BaseController {
+class CommissionController extends BaseController
+{
 
-    public function queryCommission(){
+    public function queryCommission()
+    {
 
         $filter = Input::get('filterData');
 
 
-
-        $zone = (isset($filter['zone']['zoneId']))?$filter['zone']['zoneId']:'-1';
+        $zone = (isset($filter['zone']['zoneId'])) ? $filter['zone']['zoneId'] : '-1';
         $data1 = (isset($filter['deliveryDate']) ? strtotime($filter['deliveryDate']) : strtotime("today"));
         $data2 = (isset($filter['deliveryDate1']) ? strtotime($filter['deliveryDate1']) : strtotime("today"));
 
-        $invoices =  Invoice::select(DB::raw('SUM(productQty) AS productQtys'),'productName_chi','InvoiceItem.productId','productUnitName','productQtyUnit','productPacking_carton','productPacking_inner','productPacking_unit','productPackingName_carton')->leftJoin('InvoiceItem', function($join) {
+        $invoices = Invoice::select(DB::raw('SUM(productQty) AS productQtys'), 'productName_chi', 'InvoiceItem.productId', 'productUnitName', 'productQtyUnit', 'productPacking_carton', 'productPacking_inner', 'productPacking_unit', 'productPackingName_carton')->leftJoin('InvoiceItem', function ($join) {
             $join->on('Invoice.invoiceId', '=', 'InvoiceItem.invoiceId');
-        }) ->leftJoin('Product', function($join) {
+        })->leftJoin('Product', function ($join) {
             $join->on('InvoiceItem.productId', '=', 'Product.productId');
-        }) ->groupBy('InvoiceItem.productId')->groupBy('productQtyUnit');
+        })->groupBy('InvoiceItem.productId')->groupBy('productQtyUnit');
 
-        if($zone != '-1')
-            $invoices-> where('zoneId', $zone);
+        if ($zone != '-1')
+            $invoices->where('zoneId', $zone);
         else
-            $invoices-> wherein('zoneId', explode(',', Auth::user()->temp_zone));
+            $invoices->wherein('zoneId', explode(',', Auth::user()->temp_zone));
 
-        $invoices->whereBetween('Invoice.deliveryDate', [$data1,$data2]);
+        $invoices->whereBetween('Invoice.deliveryDate', [$data1, $data2]);
 
 
-        if(Input::get('mode') == 'csv') {
+        if (Input::get('mode') == 'csv') {
             $invoices = $invoices->get()->toArray();
+//pd($invoices);
+            foreach ($invoices as &$v) {
 
-foreach($invoices as &$v){
+                if ($v['productQtyUnit'] == 'carton') {
+                    $v['commissionUnit'] = $v['productQtys'];
+                }
 
-    if($v['productQtyUnit'] == 'carton'){
-        $v['commissionUnit'] = $v['productQtys'];
-    }
+                if ($v['productQtyUnit'] == 'unit') {
+                    $v['commissionUnit'] = ($v['productQtys'] / (($v['productPacking_carton']) ? $v['productPacking_carton'] : '1' * ($v['productPacking_inner']) ? $v['productPacking_inner'] : '1' * ($v['productPacking_unit']) ? $v['productPacking_unit'] : '1'));
+                }
 
-    if($v['productQtyUnit'] == 'unit'){
-        $v['commissionUnit'] = ( $v['productQtys'] / (($v['productPacking_carton'])?$v['productPacking_carton']:'1' * ($v['productPacking_inner'])?$v['productPacking_inner']:'1' * ($v['productPacking_unit'])?$v['productPacking_unit']:'1'));
-    }
-
-    if($v['productQtyUnit'] == 'inner'){
-        $v['commissionUnit'] = ( ($v['productQtys'] * ($v['productPacking_unit'])?$v['productPacking_unit']:'1') / (($v['productPacking_carton'])?$v['productPacking_carton']:'1' * ($v['productPacking_inner'])?$v['productPacking_inner']:'1' * ($v['productPacking_unit'])?$v['productPacking_unit']:'1'));
-    }
-}
+                if ($v['productQtyUnit'] == 'inner') {
+                    $v['commissionUnit'] = (($v['productQtys'] * ($v['productPacking_unit']==false) ? 1:$v['productPacking_unit']) / (($v['productPacking_carton']) ? $v['productPacking_carton'] : '1' * ($v['productPacking_inner']) ? $v['productPacking_inner'] : '1' * ($v['productPacking_unit'] == false) ? 1 : $v['productPacking_unit']));
+                }
+            }
             $a = [];
-            foreach($invoices as $u){
+            foreach ($invoices as $u) {
                 $a[$u['productId']][] = $u;
             }
 
-
-
-            foreach( $a as &$g){
-
-                $cc =0;
-
-                foreach($g as $z){
-                   $cc += $z['commissionUnit'];
+            foreach ($a as &$g) {
+                $cc = 0;
+                foreach ($g as $z) {
+                    $cc += $z['commissionUnit'];
                 }
-
-                foreach($g as &$h){
-
-                        $h['productQtyUnit_final'] = $cc;
-
+                foreach ($g as &$h) {
+                    $h['productQtyUnit_final'] = $cc;
                 }
-
-
             }
-
-
-           return $this->exportCsv($a);
-        }  else {
+            return $this->exportCsv($a);
+        } else {
             Paginator::setCurrentPage(Input::get('start') / Input::get('length') + 1);
             $page_length = Input::get('length') <= 50 ? Input::get('length') : 50;
             $invoices = $invoices->paginate($page_length);
@@ -79,16 +69,17 @@ foreach($invoices as &$v){
 
     }
 
-    public function exportCsv($invoices){
+    public function exportCsv($invoices)
+    {
 
         $csv = 'Product ID,Name,Total Qty,Unit' . "\r\n";
         foreach ($invoices as $item) {
 
-                    $csv .= '"' . $item[0]['productId'] . '",';
-                    $csv .= '"' . $item[0]['productName_chi'] . '",';
-                    $csv .= '"' . $item[0]['productQtyUnit_final'] . '",';
-                    $csv .= '"' . $item[0]['productPackingName_carton'] . '",';
-                    $csv .= "\r\n";
+            $csv .= '"' . $item[0]['productId'] . '",';
+            $csv .= '"' . $item[0]['productName_chi'] . '",';
+            $csv .= '"' . $item[0]['productQtyUnit_final'] . '",';
+            $csv .= '"' . $item[0]['productPackingName_carton'] . '",';
+            $csv .= "\r\n";
 
         }
         echo "\xEF\xBB\xBF";
