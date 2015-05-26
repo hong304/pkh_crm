bc2494383a2aa57a56faa5550ed8e9e48bcef2bb
hong304 <hong304@gmail.com>
2015-05-26 12:27:32 +0800

    shift improvement

--- backend/app/models/ReportSettings/Invoice_1FPickingList.php
+++ backend/app/models/ReportSettings/Invoice_1FPickingList.php
@@ -1,8 +1,9 @@
 <?php
 
 
-class Invoice_1FPickingList {
-    
+class Invoice_1FPickingList
+{
+
     private $_reportTitle = "";
     private $_date = "";
     private $_zone = "";
@@ -10,117 +11,115 @@ class Invoice_1FPickingList {
     private $_invoices = [];
     private $_uniqueid = "";
     private $_version = '';
-    
+
     public function __construct($indata)
     {
-        
-        $report = Report::where('id', $indata['reportId'])->first();       
-        
+
+        $report = Report::where('id', $indata['reportId'])->first();
+
         $permittedZone = explode(',', Auth::user()->temp_zone);
 
         $this->_date = (isset($indata['filterData']['deliveryDate']) ? strtotime($indata['filterData']['deliveryDate']) : strtotime("today"));
         $this->_zone = (isset($indata['filterData']['zone']) ? $indata['filterData']['zone']['value'] : $permittedZone[0]);
-        $this->_shift =  (isset($indata['filterData']['shift']['value']))?$indata['filterData']['shift']['value']:'1';
+        $this->_shift = (isset($indata['filterData']['shift']['value'])) ? $indata['filterData']['shift']['value'] : '-1';
 
         // check if user has clearance to view this zone        
-        if(!in_array($this->_zone, $permittedZone))
-        {
+        if (!in_array($this->_zone, $permittedZone)) {
             App::abort(401, "Unauthorized Zone");
         }
         // version & id
-         $this->_uniqueid = date("Ymd", $this->_date) .  $this->_zone;
+        $this->_uniqueid = date("Ymd", $this->_date) . $this->_zone;
 
 
+        //   $lastid = ReportArchive::where('id', 'like', $this->_uniqueid.'-%-1')->select('id')->orderby('created_at', 'desc')->first();
 
-      //   $lastid = ReportArchive::where('id', 'like', $this->_uniqueid.'-%-1')->select('id')->orderby('created_at', 'desc')->first();
+        $lastid = pickingListVersionControl::where('zone', $this->_zone)->where('date', date("Y-m-d", $this->_date))->where('shift', $this->_shift)->first();
 
-        $lastid = pickingListVersionControl::where('zone',$this->_zone)->where('date',date("Y-m-d",$this->_date))->where('shift',$this->_shift)->first();
-
-      //  $lastid = @explode('-', $lastid->id);
+        //  $lastid = @explode('-', $lastid->id);
 
         $this->_version = isset($lastid->f1_version) ? $lastid->f1_version : '1';
-         
-         $this->_uniqueid = sprintf("%s-%s-1", $this->_uniqueid,  $this->_version);
 
-       $this->_reportTitle = sprintf("%s - v%s", $report->name,  $this->_version);
-       // $this->_reportTitle = sprintf("%s", $report->name);
-       // $hi = New Debug();
-       // $hi->content = $this->_reportTitle;
-      //  $hi->save();
+        $this->_uniqueid = sprintf("%s-%s-1", $this->_uniqueid, $this->_version);
+
+        $this->_reportTitle = sprintf("%s - v%s", $report->name, $this->_version);
+        // $this->_reportTitle = sprintf("%s", $report->name);
+        // $hi = New Debug();
+        // $hi->content = $this->_reportTitle;
+        //  $hi->save();
 
     }
-    
-    public function registerTitle() 
+
+    public function registerTitle()
     {
         return $this->_reportTitle;
     }
-    
+
     public function compileResults()
     {
         $date = $this->_date;
         $zone = $this->_zone;
-        
+
         // get invoice from that date and that zone
-        $this->goods = ['1F'=>[], 'version'=>[]];
-        Invoice::select('*')->where('version', true)->where('zoneId', $zone)->where('shift',$this->_shift)->where('deliveryDate', $date)->with(['invoiceItem'=>function($query){
+        $this->goods = ['1F' => [], 'version' => []];
+        $hi = Invoice::select('*')->where('version', true)->where('zoneId', $zone);
+
+        if ($this->_shift != '-1')
+            $hi->where('shift', $this->_shift);
+
+        $hi->where('deliveryDate', $date)->with(['invoiceItem' => function ($query) {
             $query->orderBy('productId')->orderBy('productQtyUnit');
         }])->with('products', 'client')
-               ->chunk(150, function($invoicesQuery){
-                   
-                   // first of all process all products
-                   $productsQuery = array_pluck($invoicesQuery, 'products');
-                   foreach($productsQuery as $productQuery)
-                   {
-                       $productQuery = head($productQuery); 
-                       foreach($productQuery as $pQ)
-                       {
-                            $products[$pQ->productId] = $pQ;
-                       }
-                   }
+            ->chunk(150, function ($invoicesQuery) {
 
-                 //  pd($invoicesQuery);
+                // first of all process all products
+                $productsQuery = array_pluck($invoicesQuery, 'products');
+                foreach ($productsQuery as $productQuery) {
+                    $productQuery = head($productQuery);
+                    foreach ($productQuery as $pQ) {
+                        $products[$pQ->productId] = $pQ;
+                    }
+                }
 
-                   // second process invoices                   
-                   foreach($invoicesQuery as $invoiceQ)
-                   {
-                       $this->_invoices[] = $invoiceQ->invoiceId;
-                       
-                       // first, store all invoices
-                       $invoiceId = $invoiceQ->invoiceId;
-                       $invoices[$invoiceId] = $invoiceQ;
-                       $client = $invoiceQ['client'];
-                       
-                       // second, separate 1F goods and 9F goods
-                       foreach($invoiceQ['invoiceItem'] as $item)
-                       {
-                           // determin its product location
-                           $productId = $item->productId;
-                           
-                           $productDetail = $products[$productId]; 
-                           $unit = $item->productQtyUnit;
-                           
-                           if($productDetail->productLocation == '1')
-                           {
-                               $this->goods['1F'][$productId][$unit] = [
-                                   'productId' => $productId,
-                                   'name' => $productDetail->productName_chi,
-                                   'unit' => $unit,
-                                   'unit_txt' => $item->productUnitName,
-                                   'counts' => (isset($this->goods['1F'][$productId][$unit]) ? $this->goods['1F'][$productId][$unit]['counts'] : 0) + $item->productQty,
-                               ];
-                           }
+                //  pd($invoicesQuery);
 
-                       }
-                   }
-                   
-               });
+                // second process invoices
+                foreach ($invoicesQuery as $invoiceQ) {
+                    $this->_invoices[] = $invoiceQ->invoiceId;
+
+                    // first, store all invoices
+                    $invoiceId = $invoiceQ->invoiceId;
+                    $invoices[$invoiceId] = $invoiceQ;
+                    $client = $invoiceQ['client'];
+
+                    // second, separate 1F goods and 9F goods
+                    foreach ($invoiceQ['invoiceItem'] as $item) {
+                        // determin its product location
+                        $productId = $item->productId;
+
+                        $productDetail = $products[$productId];
+                        $unit = $item->productQtyUnit;
+
+                        if ($productDetail->productLocation == '1') {
+                            $this->goods['1F'][$productId][$unit] = [
+                                'productId' => $productId,
+                                'name' => $productDetail->productName_chi,
+                                'unit' => $unit,
+                                'unit_txt' => $item->productUnitName,
+                                'counts' => (isset($this->goods['1F'][$productId][$unit]) ? $this->goods['1F'][$productId][$unit]['counts'] : 0) + $item->productQty,
+                            ];
+                        }
+
+                    }
+                }
+
+            });
 
 
-       ksort($this->goods['1F'],SORT_STRING);
+        ksort($this->goods['1F'], SORT_STRING);
 
-       // pd($this->goods['1F']);
+        // pd($this->goods['1F']);
 
-       $this->data = $this->goods;
+        $this->data = $this->goods;
         $this->data['version'] = $this->_version;
         $this->data['zoneId'] = $this->_zone;
         $this->data['date'] = $this->_date;
@@ -129,27 +128,26 @@ class Invoice_1FPickingList {
 
         return $this->data;
     }
-    
+
     public function registerFilter()
-    {       
-       /*
-        * Type:
-        * single-dropdown
-        * date-picker
-        * date-range
-        * single-selection
-        * multiple-selection
-        * text
-        */ 
+    {
+        /*
+         * Type:
+         * single-dropdown
+         * date-picker
+         * date-range
+         * single-selection
+         * multiple-selection
+         * text
+         */
         $zones = Zone::wherein('zoneId', explode(',', Auth::user()->temp_zone))->get();
-        foreach($zones as $zone)
-        {
+        foreach ($zones as $zone) {
             $availablezone[] = [
                 'value' => $zone->zoneId,
                 'label' => $zone->zoneName,
             ];
         }
-        $ashift =[['value'=>'1','label'=>'早班'],['value'=>'2','label'=>'晚班']];
+        $ashift = [['value' => '-1', 'label' => '檢視全部'], ['value' => '1', 'label' => '早班'], ['value' => '2', 'label' => '晚班']];
 
         $filterSetting = [
             [
@@ -173,165 +171,165 @@ class Invoice_1FPickingList {
                 'defaultValue' => date("Y-m-d", $this->_date),
             ],
         ];
-        
+
         return $filterSetting;
     }
-    
-    
-    public function beforeCompilingResults() 
+
+
+    public function beforeCompilingResults()
     {
         // executes codes before compiling results function is executed
     }
-    
+
     public function afterCompilingResults()
     {
         // executes codes after compiling results function is executed
     }
-    
+
     public function registerDownload()
     {
         $downloadSetting = [
             [
                 'type' => 'pdf',
                 'name' => '列印  PDF 版本',
-                'warning'   =>  false,
+                'warning' => false,
             ],
         ];
-        
+
         return $downloadSetting;
     }
-    
+
     public function outputPreview()
     {
 
         return View::make('reports/pickinglist1f')->with('data', $this->data)->render();
-        
+
     }
-    
-    
+
+
     # PDF Section
     public function generateHeader($pdf)
     {
-        $shift = ($this->_shift== 1)?'早班':'晚班';
-        $pdf->SetFont('chi','',18);
-        $pdf->Cell(0, 10,"炳記行貿易有限公司",0,1,"C");
-        $pdf->SetFont('chi','U',16);
-        $pdf->Cell(0, 10,$this->_reportTitle,0,1,"C");
-        $pdf->SetFont('chi','U',13);
+        if($this->_shift == 1)
+            $shift = '早班';
+        elseif($this->_shift ==2)
+            $shift = '晚班';
+        else
+            $shift = '全日';
+
+        $pdf->SetFont('chi', '', 18);
+        $pdf->Cell(0, 10, "炳記行貿易有限公司", 0, 1, "C");
+        $pdf->SetFont('chi', 'U', 16);
+        $pdf->Cell(0, 10, $this->_reportTitle, 0, 1, "C");
+        $pdf->SetFont('chi', 'U', 13);
         $pdf->Cell(0, 10, "車號: " . str_pad($this->_zone, 2, '0', STR_PAD_LEFT), 0, 2, "L");
-        $pdf->Cell(0, 5, "出車日期: " . date("Y-m-d", $this->_date)."(".$shift.")", 0, 2, "L");
+        $pdf->Cell(0, 5, "出車日期: " . date("Y-m-d", $this->_date) . "(" . $shift . ")", 0, 2, "L");
         $pdf->setXY(0, 0);
-        $pdf->SetFont('chi','', 9);
-        $pdf->Code128(10,$pdf->h-15,$this->_uniqueid,50,10);
+        $pdf->SetFont('chi', '', 9);
+        $pdf->Code128(10, $pdf->h - 15, $this->_uniqueid, 50, 10);
         $pdf->Cell(0, 10, sprintf("報告編號: %s", $this->_uniqueid), 0, 2, "R");
 
     }
-    
+
     public function outputPDF()
     {
         // Update it as generated into picking list
-       /* if(count($this->_invoices) > 0)
-        {
-            Invoice::wherein('invoiceId', $this->_invoices)->update(['invoiceStatus'=>'4']);
-        }*/
-        
-        
+        /* if(count($this->_invoices) > 0)
+         {
+             Invoice::wherein('invoiceId', $this->_invoices)->update(['invoiceStatus'=>'4']);
+         }*/
+
+
         $pdf = new PDF();
 
-        $pdf->AddFont('chi','','LiHeiProPC.ttf',true);
+        $pdf->AddFont('chi', '', 'LiHeiProPC.ttf', true);
         // handle 1F goods
         $firstF = array_chunk($this->data['1F'], 25, true);
-        
-        foreach($firstF as $i=>$f)
-        {
+
+        foreach ($firstF as $i => $f) {
             // for first Floor
             $pdf->AddPage();
 
 
             $this->generateHeader($pdf);
-        
-            $pdf->SetFont('chi','',10);
-        
+
+            $pdf->SetFont('chi', '', 10);
+
             $pdf->setXY(10, 50);
             $pdf->Cell(0, 0, "編號", 0, 0, "L");
-        
+
             $pdf->setXY(40, 50);
             $pdf->Cell(0, 0, "貨品說明", 0, 0, "L");
-        
+
             $pdf->setXY(120, 50);
             $pdf->Cell(0, 0, "訂單貨量", 0, 0, "L");
-        
+
             $pdf->setXY(145, 50);
             $pdf->Cell(0, 0, "上貨總數貨", 0, 0, "L");
-        
+
             $pdf->setXY(170, 50);
             $pdf->Cell(0, 0, "核數", 0, 0, "L");
-        
-            $pdf->Line(10, 53, 190, 53);
-        
-            $y = 60;
-        
-            $pdf->setXY(10, $pdf->h-30);
-            $pdf->Cell(0, 0, "備貨人", 0, 0, "L");
-        
-            $pdf->setXY(60, $pdf->h-30);
-            $pdf->Cell(0, 0, "核數人", 0, 0, "L");
-        
-            $pdf->Line(10, $pdf->h-35, 50, $pdf->h-35);
-            $pdf->Line(60, $pdf->h-35, 100, $pdf->h-35);
-        
-            $pdf->setXY(500, $pdf->h-30);
-            $pdf->Cell(0, 0, sprintf("頁數: %s / %s", $i+1, count($firstF)) , 0, 0, "R");
-        
-        
-            
-            foreach($f as $id=>$e)
-            {
 
-                foreach($e as $u)
-                {
+            $pdf->Line(10, 53, 190, 53);
+
+            $y = 60;
+
+            $pdf->setXY(10, $pdf->h - 30);
+            $pdf->Cell(0, 0, "備貨人", 0, 0, "L");
+
+            $pdf->setXY(60, $pdf->h - 30);
+            $pdf->Cell(0, 0, "核數人", 0, 0, "L");
+
+            $pdf->Line(10, $pdf->h - 35, 50, $pdf->h - 35);
+            $pdf->Line(60, $pdf->h - 35, 100, $pdf->h - 35);
+
+            $pdf->setXY(500, $pdf->h - 30);
+            $pdf->Cell(0, 0, sprintf("頁數: %s / %s", $i + 1, count($firstF)), 0, 0, "R");
+
+
+            foreach ($f as $id => $e) {
+
+                foreach ($e as $u) {
                     $pdf->setXY(10, $y);
                     $pdf->Cell(0, 0, $id, 0, 0, "L");
-        
+
                     $pdf->setXY(40, $y);
                     $pdf->Cell(0, 0, $u['name'], 0, 0, "L");
-        
+
                     $pdf->setXY(120, $y);
                     $pdf->Cell(0, 0, sprintf("%s", $u['counts']), 0, 0, "L");
-        
+
                     $pdf->setXY(130, $y);
                     $pdf->Cell(0, 0, str_replace(' ', '', $u['unit_txt']), 0, 0, "L");
-        
+
                     $pdf->setXY(145, $y);
                     $pdf->Cell(0, 0, "________", 0, 0, "L");
-        
+
                     $pdf->setXY(170, $y);
                     $pdf->Cell(0, 0, "________", 0, 0, "L");
-        
+
                     $y += 6;
                 }
             }
-        
+
             $y += 10;
             // Notes part
-            if($i == 0)
-            {
-                for($note=0;$note<=3;$note++)
-                {
+            if ($i == 0) {
+                for ($note = 0; $note <= 3; $note++) {
                     $pdf->Line(10, $y, 27, $y);
                     $pdf->Line(40, $y, 100, $y);
                     $pdf->Line(120, $y, 135, $y);
                     $pdf->Line(146, $y, 160, $y);
                     $pdf->Line(171, $y, 185, $y);
-            
-            
+
+
                     $y += 8;
                 }
             }
-        
+
         }
-        
-        
+
+
         // output
         return [
             'pdf' => $pdf,
