d36b4da362a00ec97dfe48a0886a5425fe8ca757
hong304 <hong304@gmail.com>
2015-05-31 18:37:56 +0800

    print queue updating

--- backend/app/controllers/PrintQueueController.php
+++ backend/app/controllers/PrintQueueController.php
@@ -379,13 +379,8 @@ class PrintQueueController extends BaseController {
         // $temp_filename = $k[0].'-'.str_pad($this->route, 2, "0", STR_PAD_LEFT).'-'.$k[1];
 
         $filename = 'pdf/'.Auth::user()->id.'-'.$invoiceImage[0]->zoneId.'-'.time().'.pdf';
-
         //$path = storage_path().'/invoices_images/'. str_replace('I', '', $k[0]) .'/'.$filename;
-
         $path = public_path($filename);
-
-
-
         $pdf->Output($path, "F");
 
         $print_log = new Printlog();
@@ -396,6 +391,30 @@ class PrintQueueController extends BaseController {
         $print_log->count = count($Ids);
         $print_log->shift = $this->shift;
         $print_log->save();
+
+        
+    }
+
+    public function sendJobViaFTP($job_id)
+    {
+        $job = Printlog::where('job_id', $job_id)->first();
+
+        $ftp_user_name = 'pkh';
+        $ftp_user_pass = 'pkh2015';
+        $ftp_server = 'pingkeehong.asuscomm.com';
+        $conn_id = ftp_connect($ftp_server);
+        ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+
+        DB::table('Printlogs')->where('job_id', $job->job_id)->update(['status'=>'sending']);
+
+        if (@ftp_put($conn_id, str_pad($job->target_path, 3, '0', STR_PAD_LEFT).'/'.$job->job_id.'-'.$job->shift.'-'.$job->count.'.pdf', $job->file_path, FTP_ASCII)) {
+            $updates = ['status'=>'downloaded;passive', 'complete_time'=>time()];
+        } else {
+            $updates = ['status'=>'fail'];
+        }
+        DB::table('Printlogs')->where('job_id', $job->job_id)->update($updates);
+        //var_dump(DB::getQUeryLog());
+        ftp_close($conn_id);
     }
 
 }
--- backend/app/models/Invoice/InvoicePrinter.php
+++ backend/app/models/Invoice/InvoicePrinter.php
@@ -6,36 +6,29 @@ class InvoicePrinter {
     public function sendJobToPrinter()
     {
         
-        $jobs = Printlog::where('job_id', '256')->get();
-
-       $this->sendJobViaFTP($jobs);
+       $this->sendJobViaFTP('341');
 
     }
     
-    public function sendJobViaFTP($jobs)
+    public function sendJobViaFTP($job_id)
     {
+        $job = Printlog::where('job_id', $job_id)->first();
+
         $ftp_user_name = 'pkh';
         $ftp_user_pass = 'pkh2015';
-        $ftp_server = '192.168.1.199';
+        $ftp_server = 'pingkeehong.asuscomm.com';
         $conn_id = ftp_connect($ftp_server);
         ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
-        
-        foreach($jobs as $job)
-        {
+
             DB::table('Printlogs')->where('job_id', $job->job_id)->update(['status'=>'sending']);
         
-            if (@ftp_put($conn_id, '000/a.pdf', $job->file_path, FTP_ASCII)) {
-                ;
-                $updates = ['status'=>'sent', 'complete_time'=>time()];
+            if (@ftp_put($conn_id, str_pad($job->target_path, 3, '0', STR_PAD_LEFT).'/'.$job->job_id.'-'.$job->shift.'-'.$job->count.'.pdf', $job->file_path, FTP_ASCII)) {
+                $updates = ['status'=>'downloaded;passive', 'complete_time'=>time()];
             } else {
                 $updates = ['status'=>'fail'];
             }
-        
-            DB::table('Printlogs')->where('job_id', $job->job_id)->update($updates);
-            // syslog(date("Y-m-d H:i", time()) . $job->invoiceId .'('.$job->job_id.')' . ' - from: ' . $job->file_path . ' -to: ' . $job->target_path, LOG_INFO);
+             DB::table('Printlogs')->where('job_id', $job->job_id)->update($updates);
             //var_dump(DB::getQUeryLog());
-        }
-        
         ftp_close($conn_id);
     }
 
