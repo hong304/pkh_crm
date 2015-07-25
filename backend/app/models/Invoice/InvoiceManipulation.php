35e6ef6f2a76c4447065e1545169688a429f5533
hong304 <hong304@gmail.com>
2015-07-25 09:33:49 +0800

    update lastitem modify time

--- backend/app/models/Invoice/InvoiceManipulation.php
+++ backend/app/models/Invoice/InvoiceManipulation.php
@@ -340,7 +340,7 @@ class InvoiceManipulation {
                 }
 
                 //update last item price
-                if($this->temp_invoice_information['status'] != '98' && $this->temp_invoice_information['status'] != '96'){
+                if($this->temp_invoice_information['status'] != '98' && $this->temp_invoice_information['status'] != '96' && $i['deleted'] == '0'){
 
                     $lastitem = lastitem::where('customerId',$this->im->customerId)->where('productId',$i['productId'])->first();
                     if($lastitem==null){
