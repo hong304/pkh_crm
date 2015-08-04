3d7483bfa5023e2c945a49c922ca3d322a4c59e4
hong304 <hong304@gmail.com>
2015-08-04 09:53:30 +0800

    dashboard

--- backend/app/views/dashboard.blade.php
+++ backend/app/views/dashboard.blade.php
@@ -3,7 +3,19 @@ $today              = strtotime("00:00:00");
 $yesterday          = strtotime("-1 day", $today);
 $tomorrow = strtotime("+1 day",$today);
 ?>
-
+<style>
+table {
+font-size:20px;
+}
+div.centre
+{
+    width: 200px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+}
+</style>
 <meta http-equiv="refresh" content="30">
 <link href="http://frontend.pingkeehong.com/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 <div class="row" style="margin: 10px">
@@ -14,15 +26,15 @@ $tomorrow = strtotime("+1 day",$today);
     <tr>
         <?php foreach($row as $k => $v):?>
 
-                      <td><?php printf('%s (%s)',$k,$v['name'])."<br/>"; ?>
-<div style="border-top: 1px solid #ddd;">
+                      <td style="background-color:<?php echo ($k%2==1)?'#FAFAFA':'#EFFBF8';?>"><div><span style="font-weight:bold"><?php printf('%s (%s)',$k,$v['name'])."<br/>"; ?></span></div>
+<div style="border-top: 1px solid #ddd;" class="centre">
                       <?php
                               foreach($v['date'] as $k1 => $v1){
                                   if($k1 == $yesterday){
-                                      echo '昨天:' . $v1['volume']. "<br/>";
+                                      echo '昨天:<span style="color: #ff0000">'.$v1['volume'].'</span><br/>';
                                   }
                                   if($k1 == $today){
-                                      echo '今天:' . $v1['volume']. "<br/>";
+                                      echo '今天:<span style="color: blue">'.$v1['volume'].'</span><br/>';
                                   }
                                   if($k1 == $tomorrow){
                                       echo '明天:' . $v1['volume']. "<br/>";
@@ -40,7 +52,7 @@ $tomorrow = strtotime("+1 day",$today);
 </div>
 
 <div class="col-md-3">
-    <table class="table table-bordered" style="font-size:15px;">
+    <table class="table table-bordered" style="font-size:20px;">
 
         <thead>
         <th class="heading">Total Volume</th>
@@ -57,13 +69,13 @@ $tomorrow = strtotime("+1 day",$today);
                     <?php
 
                         if($k == $yesterday){
-                            echo 'Yday:' . $v['volume']. "<br/>";
+                            echo '昨天:<span style="color: #ff0000">'.$v['volume'].'</span><br/>';
                         }
                         if($k == $today){
-                            echo 'Today:' . $v['volume']. "<br/>";
+                            echo '今天:<span style="color: blue">'.$v['volume'].'</span><br/>';
                         }
                         if($k == $tomorrow){
-                            echo 'Tmr:' . $v['volume']. "<br/>";
+                            echo '明天:' . $v['volume']. "<br/>";
                         }
              ?>
                 </td>
