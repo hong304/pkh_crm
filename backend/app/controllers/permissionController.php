4ff02a2975d41ef56240d95772aa62ef4111d74d
hong304 <hong304@gmail.com>
2015-08-18 20:49:41 +0800

    permission

--- backend/app/controllers/permissionController.php
+++ backend/app/controllers/permissionController.php
@@ -15,12 +15,24 @@ class permissionController extends Controller {
      */
 
     public function getPermissionList(){
+
+        if(Input::get('mode')=='posting'){
+            pd(Input::get('data'));
+        }
        // $lists = permission::with('role')->get()->toArray();
-        $lists['customer']['groupName']='客戶管理';
-        $lists['customer']['view_customer']=1;
-        $lists['customer']['edit_customer']=1;
-        $lists['customer']['create_customer']=1;
-        $lists['customer']['delete_customer']=0;
+        $lists[0]['groupName']='客戶管理';
+        $lists[0]['name']='customer';
+        $lists[0]['action']['view']=1;
+        $lists[0]['action']['edit']=1;
+        $lists[0]['action']['add']=1;
+        $lists[0]['action']['delete']=0;
+
+        $lists[1]['groupName']='產品管理';
+        $lists[1]['name']='product';
+        $lists[1]['action']['view']=1;
+        $lists[1]['action']['edit']=0;
+        $lists[1]['action']['add']=0;
+        $lists[1]['action']['delete']=0;
 
         return Response::json($lists);
     }
--- frontend/js/controllers/permissionController.js
+++ frontend/js/controllers/permissionController.js
@@ -3,21 +3,51 @@
 
 app.controller('permissionController', function($scope, $rootScope, $http, SharedService, $location, $timeout, $interval) {
 
+    $scope.permissionControl = [];
+    $scope.permissionControl_def = {
+        groupName: '',
+        action : {
+            view: '',
+            edit: '',
+            add: '',
+            delete: ''
+        }
+
+    }
+    var querytarget = endpoint + '/getPermissionLists.json';
 
     $scope.$on('$viewContentLoaded', function() {
         Metronic.initAjax();
         $scope.systeminfo = $rootScope.systeminfo;
 
-        var querytarget = endpoint + '/getPermissionLists.json';
+
 
         $http.post(querytarget, {})
             .success(function(res, status, headers, config){
-                //$scope.info = $.extend(true, {}, $scope.info_def);
-                $scope.permissionControl = res;
+
                 console.log(res);
+                res.forEach(function(item,key) {
+                    $scope.permissionControl[key] = $.extend(true, {}, $scope.permissionControl_def);
+                    $scope.permissionControl[key]['action']['view'] = item.action.view;
+                    $scope.permissionControl[key]['action']['edit'] = item.action.edit;
+                    $scope.permissionControl[key]['action']['add'] = item.action.add;
+                    $scope.permissionControl[key]['action']['delete'] = item.action.delete;
+                    $scope.permissionControl[key]['groupName'] = item.groupName;
+                    $scope.permissionControl[key]['name'] = item.name;
+                });
+
+                console.log($scope.permissionControl);
             });
 
     });
+
+    $scope.postPermission = function(){
+
+        $http.post(querytarget, {mode:'posting',data:$scope.permissionControl})
+            .success(function(res, status, headers, config){
+
+            });
+    }
 
     $scope.$watch(function() {
         return $rootScope.systeminfo;
