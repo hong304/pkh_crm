e47e8e7e98fb0944e6973ccaaba2c081a676d717
hong304 <hong304@gmail.com>
2015-09-01 17:46:00 +0800

    finance cash bug

--- frontend/js/controllers/queryInvoice.js
+++ frontend/js/controllers/queryInvoice.js
@@ -214,7 +214,7 @@ app.controller('queryInvoiceCtrl', function($scope, $rootScope, $http, SharedSer
             'clientId'		:	'0',
             'status'		:	'0',
             'zone'			:	'',
-            deliverydate : year+'-'+month+'-'+yday,
+            deliverydate : yyear+'-'+ymonth+'-'+yday,
             deliverydate2 : year+'-'+month+'-'+day,
             'created_by'	:	'0',
             'invoiceNumber' :	'',
