c98312b93e4500e10f70d5c79da9099bc3e12b5b
Hilton <hong304@gmail.com>
2015-10-20 11:44:05 +0800

    financial report

--- backend/app/controllers/financialReportController.php
+++ backend/app/controllers/financialReportController.php
@@ -1,4 +1,5 @@
 <?php
+
 class financialReportController extends BaseController
 {
 
@@ -10,26 +11,24 @@ class financialReportController extends BaseController
 
         if (Input::get('mode') == 'csv') {
 
-            $ymd =Input::get('filterData.deliveryDate');
-
+            $ymd = Input::get('filterData.deliveryDate');
 
 
             $time_interval = [['0', '0'], ['1', '1'], ['2', '2'], ['5', '3'], ['11', '6'], ['120', '12']];
-           $dateRange = ['F', 'G', 'H', 'I', 'J', 'K'];
+            $dateRange = ['C', 'D', 'E', 'F', 'G', 'H','I'];
 
             $first = true;
 
             foreach ($time_interval as $v) {
                 if ($first) {
-                    $time[date("Y-m", strtotime($ymd."-" . $v[1] . " month"))][0] = date("Y-m-01", strtotime($ymd."-" . $v[0] . " month"));
-                    $time[date("Y-m", strtotime($ymd."-" . $v[1] . " month"))][1] = date("Y-m-d", strtotime($ymd."-" . $v[1] . " month"));
+                    $time[date("Y-m", strtotime($ymd . "-" . $v[1] . " month"))][0] = date("Y-m-01", strtotime($ymd . "-" . $v[0] . " month"));
+                    $time[date("Y-m", strtotime($ymd . "-" . $v[1] . " month"))][1] = date("Y-m-d", strtotime($ymd . "-" . $v[1] . " month"));
                     $first = false;
                 } else {
-                    $time[date("Y-m", strtotime($ymd."-" . $v[1] . " month"))][0] = date("Y-m-01", strtotime($ymd."-" . $v[0] . " month"));
-                    $time[date("Y-m", strtotime($ymd."-" . $v[1] . " month"))][1] = date("Y-m-t", strtotime($ymd."-" . $v[1] . " month"));
+                    $time[date("Y-m", strtotime($ymd . "-" . $v[1] . " month"))][0] = date("Y-m-01", strtotime($ymd . "-" . $v[0] . " month"));
+                    $time[date("Y-m", strtotime($ymd . "-" . $v[1] . " month"))][1] = date("Y-m-t", strtotime($ymd . "-" . $v[1] . " month"));
                 }
             }
-
 
 
             $month[0] = key(array_slice($time, -6, 1, true));
@@ -40,28 +39,32 @@ class financialReportController extends BaseController
             $month[5] = key(array_slice($time, -1, 1, true));
 
 
-              //  $total = 0;
-                for($i = 0; $i < 21 ;$i++) {
+            //  $total = 0;
+         /*   for ($i = 0; $i < 23; $i++) {
 
-                    //for ($i = $this->_reportMonth; $i > 0; $i--) {
-                    $data = [];
+                //for ($i = $this->_reportMonth; $i > 0; $i--) {
+                $data = [];
 
-                    foreach ($time as $k => $v) {
-                        $data[$k] = Invoice::whereBetween('deliveryDate', [strtotime($v[0]), strtotime($v[1])])->where('paymentTerms', 1)->where('amount', '!=', DB::raw('paid'))->where('manual_complete', false)->whereNotIn('invoiceStatus',['2','30','99'])->where('zoneId',$i)->OrderBy('deliveryDate')->get();
+                foreach ($time as $k => $v) {
+                    $data[$k] = Invoice::whereBetween('deliveryDate', [strtotime($v[0]), strtotime($v[1])])->where('paymentTerms', 1)->where('amount', '!=', DB::raw('paid'))->where('manual_complete', false)->whereNotIn('invoiceStatus', ['2', '30', '99'])->where('zoneId', $i)->OrderBy('deliveryDate')->get();
 
-                        foreach ($data[$k] as $invoice) {
+                    foreach ($data[$k] as $invoice) {
 
-                            if (!isset($this->_monthly[$i][$k]))
-                                $this->_monthly[$i][$k] = 0;
+                        if (!isset($this->_monthly[$i][$k]))
+                            $this->_monthly[$i][$k] = 0;
 
-                            $this->_monthly[$i][$k] += ($invoice->amount - ($invoice->paid + $invoice->discount_taken));
-                           // $total += ($invoice->realAmount - ($invoice->paid + $invoice->discount_taken));
+                        $this->_monthly[$i][$k] += ($invoice->amount - ($invoice->paid + $invoice->discount_taken));
+                        // $total += ($invoice->realAmount - ($invoice->paid + $invoice->discount_taken));
 
-                        }
                     }
-
                 }
-            }
+
+            } */
+
+        }
+
+        $value = debug::where('id',1)->first();
+        $this->_monthly = json_decode($value->content);
 
 
         $i = 5;
@@ -81,81 +84,62 @@ class financialReportController extends BaseController
 
         $objPHPExcel->getActiveSheet()->setCellValue('A' . $i, "車線");
 
-        $objPHPExcel->getActiveSheet()->setCellValue('E' . $i, "總和");
-        $objPHPExcel->getActiveSheet()->setCellValue('F' . $i, $month[0]);
-        $objPHPExcel->getActiveSheet()->setCellValue('G' . $i, $month[1]);
-        $objPHPExcel->getActiveSheet()->setCellValue('H' . $i, $month[2]);
-        $objPHPExcel->getActiveSheet()->setCellValue('I' . $i, $month[3]);
-        $objPHPExcel->getActiveSheet()->setCellValue('J' . $i, $month[4]);
-        $objPHPExcel->getActiveSheet()->setCellValue('K' . $i, $month[5]);
+        $objPHPExcel->getActiveSheet()->setCellValue('C' . $i, $month[0]);
+        $objPHPExcel->getActiveSheet()->setCellValue('D' . $i, $month[1]);
+        $objPHPExcel->getActiveSheet()->setCellValue('E' . $i, $month[2]);
+        $objPHPExcel->getActiveSheet()->setCellValue('F' . $i, $month[3]);
+        $objPHPExcel->getActiveSheet()->setCellValue('G' . $i, $month[4]);
+        $objPHPExcel->getActiveSheet()->setCellValue('H' . $i, $month[5]);
+        $objPHPExcel->getActiveSheet()->setCellValue('I' . $i, "Total");
 
 
         $j = $i + 1;
         $storeRow = $j;
 
-        $a = $j;
-        $b = $j;
-
-
 
         foreach ($this->_monthly as $ks => $vs) {
 
+            $objPHPExcel->getActiveSheet()->setCellValue('A' . $j, $ks);
+
+             $yy = 0;
 
 
-                    $c = $j;
-                    foreach ($vs as $g => $k) {
-                        //  $objPHPExcel->getActiveSheet()->setCellValue('A' . $j, $g);
-
-                        for ($yy = 0; $yy < count($dateRange); $yy++) {
-                            if ($ks == $month[$yy]) {
-                                $objPHPExcel->getActiveSheet()->setCellValue($dateRange[$yy] . $c, $vs['byCustomer'][$g]);
-                                $c++;
-                            }
-                        }
-
-
+            foreach($month as $v){
+                    if(isset($vs->$v)){
+                        $objPHPExcel->getActiveSheet()->setCellValue($dateRange[$yy] . $j, $vs->$v);
+                    }
+                $yy++;
             }
-            // $j++;
+
+            $objPHPExcel->getActiveSheet()->setCellValue('I' . $j, "=SUM(C" . $j . ":H" . $j . ")");
+             $j++;
         }
-        for($loopNum = 0;$loopNum<count($storeDate);$loopNum++)
-        {
-            $objPHPExcel->getActiveSheet()->setCellValue('E' . $j,"=SUM(F".$j.":K".$j.")");
-            $j++;
-        }
+
         $j++;
+
 
 
         $objPHPExcel->getActiveSheet()->setCellValue('A' . $j, "合共總額:");
 
-        $hh = $j -2;
+        $hh = $j - 2;
 
         $styleArray = array(
             'font' => array(
                 'underline' => PHPExcel_Style_Font::UNDERLINE_DOUBLE
             )
         );
-        $dateRange[6] = "E";
-        for($count = 0;$count < count($dateRange);$count++)
-        {
-            $objPHPExcel->getActiveSheet()->setCellValue($dateRange[$count].  $j,"=SUM(".$dateRange[$count].$storeRow.":".$dateRange[$count].$hh.")");
-            $objPHPExcel->getActiveSheet()->getStyle($dateRange[$count].  $j)->applyFromArray($styleArray);
+
+        for ($count = 0; $count < count($dateRange); $count++) {
+            $objPHPExcel->getActiveSheet()->setCellValue($dateRange[$count] . $j, "=SUM(" . $dateRange[$count] . $storeRow . ":" . $dateRange[$count] . $hh . ")");
+            $objPHPExcel->getActiveSheet()->getStyle($dateRange[$count] . $j)->applyFromArray($styleArray);
         }
 
         unset($styleArray);
-        /*      $data = array(
-          array ('Name', 'Surname'),
-          array('Schwarz', 'Oliver'),
-          array('Test', 'Peter')
-      );
-      $objPHPExcel->getActiveSheet()->fromArray($data, null, 'G1');*/
 
-        //foreach() {
-
-        // }
 
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
         header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="' . "abc" . '.xls"');
+        header('Content-Disposition: attachment;filename="' . "agingByZoneCash" .$ymd. '.xls"');
         header('Cache-Control: max-age=0');
         $objWriter->save('php://output');
 
