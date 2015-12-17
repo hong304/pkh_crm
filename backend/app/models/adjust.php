5334f590d40363e2d51e892577cc10f7a71f4026
hong304 <hong304@gmail.com>
2015-12-17 15:22:42 +0800

    inventory Adjustment

--- backend/app/models/adjust.php
+++ backend/app/models/adjust.php
@@ -3,20 +3,6 @@
 
 class adjust extends Eloquent  {
 
-
-    public static function boot()
-    {
-        parent::boot();
-
-        static::updating(function($table)  {
-            $table->updated_by = Auth::user()->id;
-        });
-        static::saving(function($table)  {
-            $table->updated_by = Auth::user()->id;
-        });
-
-    }
-
     public function newReceiving()
     {
         return $this->hasOne('Receiving', 'adjustId', 'adjustId');
