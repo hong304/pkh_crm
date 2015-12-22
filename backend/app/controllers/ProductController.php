39f270bf0f943a5faf3e6f4e6cf5afb993545bec
hong304 <hong304@gmail.com>
2015-12-22 16:35:54 +0800

    product edit

--- backend/app/controllers/ProductController.php
+++ backend/app/controllers/ProductController.php
@@ -363,7 +363,7 @@ class ProductController extends BaseController {
             return Datatables::of($product)
                 ->addColumn('link', function ($p) {
                   //  if(Auth::user()->can('edit_product'))
-                    if(Auth::user()->id == 8 || Auth::user()->id == 9 || Auth::user()->id == 46)
+                    if(Auth::user()->id == 8 || Auth::user()->id == 9 || Auth::user()->id == 46|| Auth::user()->id == 55)
                     return '<span onclick="editProduct(\''.$p->productId.'\')" class="btn btn-xs default"><i class="fa fa-search"></i> 修改</span>';
                         else
                     return '';
